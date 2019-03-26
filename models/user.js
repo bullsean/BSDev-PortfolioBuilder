b56ca8dcad12c655d415278dbf88ea199e658ca6
bullsean <41093836+bullsean@users.noreply.github.com>
2019-03-25 21:10:29 -0500

    Added all models and associations, as well as the user input

--- models/user.js
+++ models/user.js
@@ -2,39 +2,60 @@ module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     firstName: {
         type: DataTypes.STRING,
-        validate: {
-            isNull: false,
-            len: [1]
-        }
+        // validate: {
+        //     isNull: false,
+        //     len: [1]
+        // }
     },
     lastName: {
         type: DataTypes.STRING,
-        validate: {
-            isNull: false,
-            len: [1]
-        }
+        // validate: {
+        //     isNull: false,
+        //     len: [1]
+        // }
     },
     password: {
         type: DataTypes.STRING,
-        validate: {
-            isNull: false,
-            len: [8]
-        }
+        // validate: {
+        //     isNull: false,
+        //     len: [8]
+        // }
     },
     email: {
         type: DataTypes.STRING,
-        validate: {
-            isEmail: true
-        }
+        // validate: {
+        //     isEmail: true
+        // }
     },
 });
-// User.associate = function (models) {
-//     // Associating Author with Posts
-//     // When an Author is deleted, also delete any associated Posts
-//     User.hasMany(models.Post, {
-//         onDelete: "cascade"
-//     });
-// };
+
+User.associate = function (models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Project, {
+        onDelete: "cascade"
+    });
+
+    User.hasOne(models.ProfileName, {
+        onDelete: "cascade"
+    });
+
+    User.hasMany(models.Certifications, {
+        onDelete: "cascade"
+    });
+
+    User.hasOne(models.ContactLinks, {
+        onDelete: "cascade"
+    });
+
+    User.hasMany(models.Education, {
+        onDelete: "cascade"
+    });
+
+    User.hasMany(models.Skills, {
+        onDelete: "cascade"
+    });
+};
 
   return User;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,8 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   //reCaptcha feature
-  app.post("/submit", function(req, res) {
+  app.post("/submit", function (req, res) {
     // g-recaptcha-response is the key that browser will generate upon form submit.
     // if its blank or null means user has not selected the captcha, so return the error.
     if (
@@ -26,7 +26,7 @@ module.exports = function(app) {
       "&remoteip=" +
       req.connection.remoteAddress;
     // Hitting GET request to the URL, Google will respond with success or error scenario.
-    request(verificationUrl, function(_error, _response, body) {
+    request(verificationUrl, function (_error, _response, body) {
       body = JSON.parse(body);
       // Success will be true or false depending upon captcha validation.
       if (body.success !== undefined && !body.success) {
@@ -36,6 +36,23 @@ module.exports = function(app) {
         });
       }
       res.json({ responseCode: 0, responseDesc: "Sucess" });
+    });
+  });
+
+  app.post("/api/users", function(req, res) {
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var password = req.body.password;
+    var email = req.body.email;
+
+    db.User.create({
+      firstName: firstName,
+      lastName: lastName,
+      password: password,
+      email: email
+    }).then(function(result) {
+      // We have access to the new todo as an argument inside of the callback function
+      res.json(result);
     });
   });
 
