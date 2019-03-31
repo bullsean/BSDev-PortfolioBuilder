65af6bae0c9d61905056cbea1838a82e61c66442
HaifaSeyam <h.abuamro@hotmail.com>
2019-03-30 23:04:52 -0500

    validation and hiding tables done -- social media part not done yet

--- public/js/main.js
+++ public/js/main.js
@@ -20,38 +20,6 @@ $(document).ready(function() {
     }
   });
 
-  //On submiting user signup info
-  $("#signUp").on("click", function(event) {
-    event.preventDefault();
-    //1. get user input
-    var password = $("#password")
-      .val()
-      .trim();
-    var email = $("#email")
-      .val()
-      .trim();
-    var uid = $("data").data("uid")
-    //validate user input
-    if (signupValidation(userName, password, email)) {
-      //if user input is valid .. send data to the (authintication methods - post action below --
-      //check the api-routes file --- we do not need any html route for "/signup" rather we will render "/dashboard"
-      //if authintication success .. redirect the user to his dashboard where his username will be displayed
-      $.ajax({
-        url: "/api/account",
-        method: "POST",
-        data: {
-          email: email,
-          password: password,
-          uid: uid
-        }
-      }).then(function(data) {
-        window.location = "/account" + "/" + data.id;
-      });
-    } else {
-      return false;
-    }
-  });
-
   var userId = $("#data").data("id");
   //When user submit his name, these info will be sent to the server -- check api-routes file
   $("#nameSubmit").on("click", function(event) {
@@ -208,21 +176,35 @@ $(document).ready(function() {
     }
   });
 
-  // $(".uploadImage").on("click", function(event) {
-  //   event.preventDefault();
-  //     $.ajax({
-  //       url: "/api/upload/" + userId,
-  //       method: "POST",
-  //       data: {
-  //         userId: userId
-  //       }
-  //     }).then(function() {
-  //       location.reload();
-  //     });
-  // });
+  //When updateExp button clicked update the record in DB
+  $(".updateName").on("click", function() {
+    var idToUpdate = $(this).data("updateid");
+    event.preventDefault();
+    var firstName = $("#first_name")
+      .val()
+      .trim();
+    var lastName = $("#last_name")
+      .val()
+      .trim();
+    var title = $("#title")
+      .val()
+      .trim();
 
-
-
+    if (validateName(firstName, lastName, title)){
+      // Send the PUT request.
+      $.ajax("/api/updateName/" + idToUpdate, {
+        type: "PUT",
+        data: {
+          firstName: firstName,
+          lastName: lastName,
+          title: title
+        }
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
+  });
   //When updateExp button clicked update the record in DB
   $(".updateExp").on("click", function() {
     var idToUpdate = $(this).data("updateid");
@@ -237,18 +219,20 @@ $(document).ready(function() {
       .val()
       .trim();
 
-    // Send the PUT request.
-    $.ajax("/api/updateExp/" + idToUpdate, {
-      type: "PUT",
-      data: {
-        comJectName: comJectName,
-        titleRole: titleRole,
-        desc: desc
-      }
-    }).then(function() {
-      // Reload the page to get the updated list
-      location.reload();
-    });
+    if (validateExperiences(comJectName, titleRole, desc)) {
+      // Send the PUT request.
+      $.ajax("/api/updateExp/" + idToUpdate, {
+        type: "PUT",
+        data: {
+          comJectName: comJectName,
+          titleRole: titleRole,
+          desc: desc
+        }
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
   });
 
   //When updateEdu button clicked update the record in DB
@@ -262,17 +246,19 @@ $(document).ready(function() {
       .val()
       .trim();
 
-    // Send the PUT request.
-    $.ajax("/api/updateEdu/" + idToUpdate, {
-      type: "PUT",
-      data: {
-        institution: institution,
-        degree: degree
-      }
-    }).then(function() {
-      // Reload the page to get the updated list
-      location.reload();
-    });
+    if (validateEducation(institution, degree)) {
+      // Send the PUT request.
+      $.ajax("/api/updateEdu/" + idToUpdate, {
+        type: "PUT",
+        data: {
+          institution: institution,
+          degree: degree
+        }
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
   });
 
   //When updateLicert button clicked update the record in DB
@@ -283,16 +269,18 @@ $(document).ready(function() {
       .val()
       .trim();
 
-    // Send the PUT request.
-    $.ajax("/api/updateLicert/" + idToUpdate, {
-      type: "PUT",
-      data: {
-        licertName: licertName
-      }
-    }).then(function() {
-      // Reload the page to get the updated list
-      location.reload();
-    });
+    if (validateLicert(licertName)) {
+      // Send the PUT request.
+      $.ajax("/api/updateLicert/" + idToUpdate, {
+        type: "PUT",
+        data: {
+          licertName: licertName
+        }
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
   });
 
   //When updateSkaccom button clicked update the record in DB
@@ -303,38 +291,19 @@ $(document).ready(function() {
       .val()
       .trim();
 
-    // Send the PUT request.
-    $.ajax("/api/updateSkaccom/" + idToUpdate, {
-      type: "PUT",
-      data: {
-        skaccomName: skaccomName
-      }
-    }).then(function() {
-      // Reload the page to get the updated list
-      location.reload();
-    });
+    if (validateSkaccom(skaccomName)) {
+      // Send the PUT request.
+      $.ajax("/api/updateSkaccom/" + idToUpdate, {
+        type: "PUT",
+        data: {
+          skaccomName: skaccomName
+        }
+      }).then(function() {
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
   });
-
-  // //When updateConnect button clicked update the record in DB
-  // $(".updateConnect").on("click", function() {
-  //   var idToUpdate = $(this).data("updateid");
-  //   console.log(idToUpdate);
-  //   event.preventDefault();
-  //   var facebook = $("#facebook")
-  //     .val()
-  //     .trim();
-
-  //   // Send the PUT request.
-  //   $.ajax("/api/updateConnect/" + idToUpdate, {
-  //     type: "PUT",
-  //     data: {
-  //       facebook: facebook
-  //     }
-  //   }).then(function() {
-  //     // Reload the page to get the updated list
-  //     location.reload();
-  //   });
-  // });
 
   //When DeleteExp button clicked delete the record from DB
   $(".deleteExp").on("click", function() {
@@ -395,50 +364,6 @@ $(document).ready(function() {
       location.reload();
     });
   });
-
-  // //On submiting profile image through account page
-  // $("#profileImgSubmit").on("click", function(event){
-  //   //to be done later
-  // });
-
-  //Update button
-  // $("#update").on("click", function(event){
-  //   event.preventDefault();
-  //   var updatedItem = $(this).val().trim();
-
-  //     $.ajax({
-  //       method: "PUT",
-  //       url: "/api/update" + userId,
-  //       data: updatedItem
-  //     }).then(function() {
-  //       location.reload();
-  //     });
-  // });
-
-  // //Validation functions
-  function signupValidation(un, pass, email) {
-    if (un.length < 1) {
-      $("#username").css("border", "1px solid red");
-    } else {
-      $("#username").css("border", "");
-    }
-
-    if (pass.length < 8) {
-      $("#password").css("border", "1px solid red");
-    } else {
-      $("#password").css("border", "");
-    }
-
-    if (email.length < 1) {
-      $("#email").css("border", "1px solid red");
-    } else {
-      $("#email").css("border", "");
-    }
-
-    if (un.length > 1 && pass.length > 8 && email.length > 0) {
-      return true;
-    }
-  }
 
   //Validate profile info
   function validateName(fn, ln, title) {
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,21 +1,21 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     res.render("index", {
       title: "Home | Portfolio Creator"
     });
   });
 
   //When user click on login at the navbar he will be directed to the login page
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     res.render("login", {
       title: "Log In | Portfolio Creator"
     });
   });
   //When user click on dark template image in the main page he will be directed to the dark template page
-  app.get("/dark/:id", function(req, res) {
+  app.get("/dark/:id", function (req, res) {
     db.User.findOne({
       include: [
         db.ProfileName,
@@ -37,21 +37,12 @@ module.exports = function(app) {
 
   });
   //When user click on light template image in the main page he will be directed to the light template page
-  app.get("/light", function(req, res) {
+  app.get("/light", function (req, res) {
     res.render("light", {
       layout: "templates",
       title: "Light Template | Portfolio Creator"
     });
   });
-
-  // app.get("/account/:id", function (req, res) {
-  //   res.render("account", {
-  //     title: "Account | Portfolio Creator",
-  //     data: {
-  //       id: req.params.id
-  //     }
-  //   });
-  // });
 
   app.get("/account/:id", function(req, res) {
     var userId = req.params.id;
@@ -75,9 +66,9 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/api/profileName/:UserId", function(req, res) {
+  app.get("/api/profileName/:UserId", function (req, res) {
     //Selecting user signup/data to display in account page
-    db.User.findOne({ where: { id: req.params.UserId } }).then(function(results) {
+    db.User.findOne({ where: { id: req.params.UserId } }).then(function (results) {
       var fullName = results.profileFirstName + " " + results.profileLastName;
       res.render("dark", {
         title: fullName + " | Portfolio Creator",
@@ -85,22 +76,4 @@ module.exports = function(app) {
       });
     });
   });
-
-  //   db.User.findOne({
-  //     where: {
-  //       id: req.params.id
-  //     }
-  //   }).then(function(result) {
-  //       console.log(result);
-  //       // console.log(result[0].dataValues);
-  //       // console.log(result[0].dataValues.firstName);
-  //       res.render("dashboard", {
-  //         title: "Dashboard | Portfolio Creator",
-  //         data: result.dataValues
-  //       });
-  //     });
-  //   res.render("dashboard", {
-  //     title: "Dashboard | Portfolio Creator"
-  //   });
-  // });
 };
